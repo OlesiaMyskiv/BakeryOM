70612c4f82c42cee6690e30f98f16115e6108fa2
Olesia <myskiv12385@gmail.com>
2026-04-08 20:33:04 +0300

    commit

--- src/main/java/OM/Bakery/repository/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package OM.Bakery.repository;
-
-import OM.Bakery.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Інтерфейс репозиторію для сутності Review.
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
